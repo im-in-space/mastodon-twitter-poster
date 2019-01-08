76b89ab9ec33535de9f2917d1e9c315d61ef2326
MKody <im@kdy.ch>
2019-01-08 10:55:11 +0100

    Set older ruby version and add sidekiq ui

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])) &
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
+  end if Rails.env.production?
+  mount Sidekiq::Web, at: "/sidekiq"
 
   resource :account
   resources :authorizations, only: [:destroy]
